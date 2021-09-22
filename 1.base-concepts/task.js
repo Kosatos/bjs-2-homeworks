9e35249ed4dc1ae615b9c2ac3040119f2aab8c60
Konstantin Muzyka <kosatkinms@mail.ru>
2021-09-23 02:11:05 +0300

    1 задание Основные понятия

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,29 @@
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
+  'use strict'
+  let arr = [];
+  let d = b ** 2 - 4 * a * c;
+  let x1;
+  let x2;
+  if (d < 0) {
+    arr = arr;
+  } else if (d === 0) {
+    x1 = -b / (2 * a);
+    arr.push(x1);
+  } else {
+    x1 = (-b + Math.sqrt(d) ) / (2 * a);
+    x2 = (-b - Math.sqrt(d) ) / (2 * a);
+    arr.push(x1, x2);
+  }
   return arr; // array
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
+  'use strict'  
   let totalAmount;
-
-  // код для задачи №2 писать здесь
-
-  return totalAmount;
+  percent = +percent / 100;
+  let creditBody = Number(amount) - Number(contribution);
+  let months = date.getDate();
+  let monthlyPayment = (creditBody * ((percent / 12) + (percent / 12) / (((1 + (percent / 12)) ** months) - 1)));
+  totalAmount = +contribution + (monthlyPayment * months) + (percent * amount);
+  return totalAmount.toFixed(2);
 }
