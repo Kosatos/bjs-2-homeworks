a3fff70038de0e6a5cdbf9254f4e85bdfaab3905
Konstantin Muzyka <kosatkinms@mail.ru>
2021-09-27 02:42:47 +0300

    Задачи 1, 2 по теме "Функции"

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,27 +1,44 @@
 // Задание 1
 function getArrayParams(arr) {
   let min,max,sum,avg;
+    min = arr[0];
+    max = arr[0];
+    sum = 0;
 
-  // Ваш код
+  for(let i = 0; i < arr.length; i++) {
+    arr[i] > max ? max = arr[i] : max = max;
+    arr[i] < min ? min = arr[i] : min = min;
+    sum += arr[i];
 
-  return { min:min, max:max, avg:avg };
+    if (arr[i] < -100 || arr[i] > 100) {
+      const warning = 'Введите число в диапазоне от -100 до 100';
+      return warning;
+    }
+  }
+
+  avg = +(sum / arr.length).toFixed(2);
+  return { min:min, max:max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
+  let sum = 0;
 
-  // Ваш код
+  for(let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
 
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max
+  let max = -Infinity;
 
-  // Ваш кода
-  // for ...
-  
+  for(let i = 0; i < arrOfArr.length; i++) {
+    const sum = func(arrOfArr[i]);
+    sum > max ? max = sum : max = max;
+  }
+
   return max
 }
 
