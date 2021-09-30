c8ff7567c10b1f7a3226d292086099a321578ae6
Константин Музыка <86382755+Kosatos@users.noreply.github.com>
2021-09-30 11:20:17 +0300

    Доработка первой задачи по arrays

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,10 +1,7 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  result = arr1.every((idx) => arr1[idx] === arr2[idx]);
-
-  return result; // boolean
+  return arr1.every((item, idx) => item === arr2[idx] && arr1.length === arr2.length); 
 }
+
 
 function advancedFilter(arr) {
   let resultArr;
