5da009a03919e0cd8b3297d72305160f88748eb4
Konstantin Muzyka <kosatkinms@mail.ru>
2021-10-13 00:32:38 +0300

    Решение задач 1 и 2 по exception-closure

---
+++ 6.exception-closure/task.js
@@ -0,0 +1,55 @@
+// Задача 1
+
+const parseCount = (value) => {
+    if (isNaN(parseInt(value, 10))) {
+        throw new Error('Невалидное значение');
+    }
+    return parseInt(value, 10);
+}
+
+const validateCount = (value) => {
+    try {
+        return parseCount(value);
+    } catch(err) {
+        return err;
+    }
+}
+
+// Задача 2
+
+class Triangle {
+    constructor(a, b, c) {
+        if ((a + b) < c || (a + c) < b || (b + c) < a) {
+            throw new Error('Треугольник с такими сторонами не существует');
+        } else if (isNaN(parseInt(a, 10)) || isNaN(parseInt(b, 10)) || isNaN(parseInt(c, 10))) {
+            throw new Error('Невалидное значение стороны трекгольника');
+        }
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    getPerimeter() {
+        return this.perimeter = this.a + this.b + this.c;
+    } 
+
+    getArea() {
+        const semiperimeter = this.perimeter / 2;
+        return this.area = +((Math.sqrt(semiperimeter * (semiperimeter - this.a) * (semiperimeter - this.b) * (semiperimeter - this.b))).toFixed(3));
+    }
+}
+
+const getTriangle = (a, b, c) => {
+    try {
+        return new Triangle(a, b, c);
+    } catch(err) {
+        return {
+            getArea() {
+                return console.error('Ошибка! Треугольник не существует');
+            },
+            getPerimeter() {
+                return console.error('Ошибка! Треугольник не существует');
+            },
+        }
+    }
+}
