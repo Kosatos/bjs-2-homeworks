6ce10cd5fa1d5ef370c4f5939816b10d5b353d8f
Konstantin Muzyka <kosatkinms@mail.ru>
2021-10-19 00:37:02 +0300

    decorators

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,53 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+  
+  return function (...args) {
+    const hash = args.toString();  
+    let idx = cache.findIndex(item => item.hash === hash); 
+    if (idx !== -1 ) { 
+      return `Из кэша: ${cache[idx].result}`;
+    } else {
+      let result = func.call(this, ...args); 
+      cache.push({hash, result}) ; 
+      if (cache.length > 5) { 
+        cache.shift(); 
+      }
+      return `Вычисляем: ${result}`;  
+    }
+  }
 }
 
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+function debounceDecoratorNew(func, ms) {
+  let isDelayed = false;
+
+  return function(...args) {
+    if (!isDelayed) {
+      func.call(this, ...args);
+      isDelayed = true;
+
+      setTimeout(() => {
+        isDelayed = false;
+      }, ms);
+    }
+  }
 }
 
-function debounceDecorator2(func) {
-  // Ваш код
+function debounceDecoratorNew2(func, ms) {
+  let isDelayed = false;
+  let count = 0;
+
+  return function wrapper(...args) {
+    wrapper.count = count++;
+ 
+    if (!isDelayed) {
+      func.call(this, ...args);
+      isDelayed = true;
+
+      setTimeout(() => {
+        isDelayed = false;
+      }, ms);
+    }
+  }
 }
+
